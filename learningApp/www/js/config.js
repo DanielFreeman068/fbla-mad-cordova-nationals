f687f150acd6b0fdf76b2cb3c7c5604982c79079
DerpNerd <sanchez1.alan1@gmail.com>
2025-03-21 13:27:40 -0700

    Fixed

--- learningApp/www/js/config.js
+++ learningApp/www/js/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // IP: "https://adolescent-accessing-fought-naval.trycloudflare.com",
-    IP: "http://localhost:3000"
+    IP: "https://need-veterinary-arrested-filled.trycloudflare.com",
+    // IP: "http://localhost:3000"
   };
 
   export default config;
